9e967e2762afaf70e9d9a8779995c35f96a3c464
Mark <hernandeztzin@gmail.com>
2024-12-31 17:57:52 -0600

    CRUD modelo BD

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 
 
+use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::resource('post', PostController::class);
